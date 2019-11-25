c36669c8a859d37912365d38d3bb88178659951d
ianhunterharold <ianhunterharold@gmail.com>
2019-11-24 22:06:51 -0800

    added destroy and starting to build out create backend

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,13 +1,33 @@
 class GroupsController < ApplicationController
-  before_action :authorized, only: [:create, :edit]
+  before_action :authorized, only: [:create, :edit, :destroy]
 
   def index
     render json: Group.all
   end 
 
   def edit
-    
+  end 
+
+  def create
+    @group = Group.create(group_params)
+    render json: @group
+  end 
+
+
+
+  def destroy 
+    @group = id_params 
+    @group.destroy
   end 
   
-  
+  private 
+
+  def id_params 
+    @group = Group.find(params[:id])
+  end 
+
+  def group_params 
+    params.require(:group).permit(:group_id)
+  end 
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :password, presence: true
-  validates :username, presence: true
-  validates :username, uniqueness: { case_sensative: false }
   has_many :karmas
   has_many :groups
   has_many :user_groups
   has_many :groups, through: :user_groups
+  validates :password, presence: true
+  validates :username, presence: true
+  validates :username, uniqueness: { case_sensative: false }
 end
